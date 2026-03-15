ffc0e6b47cd44dda86b6bbd2a2618f3f0634b50e
SuperS <guillaumedl59@gmail.com>
2026-03-15 22:20:57 +0100

    add codemagic workflow for ios build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kmroulant.KmRoulant',
+  appName: 'KmRoulant',
+  webDir: 'build'
+};
+
+export default config;
